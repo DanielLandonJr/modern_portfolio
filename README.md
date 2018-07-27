14bb9cdffa84977dd24312e5fad464255f61bd36
Daniel C. Landon Jr <daniel.landonjr@gmail.com>
2018-07-27 04:39:00 -0400

    Added side menu button and animation that goes with it.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# profile
+# Portfolio
+
+Attempt, using video series from Traversy media, to create a visually appealing portfolio.
+
+I am good at the back end of things, what goes on under the hood. At this point my visual design skills are some-what lacking...this I am working on. For now this will work nicely for what I need.
--- js/main.js
+++ js/main.js
@@ -1,3 +1,50 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('javascript loaded ...');
-});
+const Portfolio = (() => {
+  // not had good luck doing this...will have to look into why
+  const menuBtn = document.querySelector('.menu_button');
+  const menu = document.querySelector('.menu');
+  const menuNav = document.querySelector('.menu_nav');
+  const menuBranding = document.querySelector('.menu_branding');
+  const menuItems = document.querySelectorAll('.menu_nav-item');
+
+  // initial state of menu
+  let showMenu = false;
+
+  const loadEventListeners = () => {
+    menuBtn.addEventListener('click', toggleMenu);
+  };
+
+  const toggleMenu = () => {
+    if (!showMenu) {
+      menuBtn.classList.add('close');
+      menu.classList.add('show');
+      menuNav.classList.add('show');
+      menuBranding.classList.add('show');
+      menuItems.forEach(item => item.classList.add('show'));
+
+      // set menu state
+      showMenu = true;
+    } else {
+      menuBtn.classList.remove('close');
+      menu.classList.remove('show');
+      menuNav.classList.remove('show');
+      menuBranding.classList.remove('show');
+      menuItems.forEach(item => item.classList.remove('show'));
+
+      // set menu state
+      showMenu = false;
+    }
+  };
+
+  document.addEventListener('DOMContentLoaded', () => {
+    console.log('javascript loaded ...');
+  });
+
+  // public methods
+  return {
+    init: () => {
+      loadEventListeners();
+    }
+  };
+})();
+
+Portfolio.init();
